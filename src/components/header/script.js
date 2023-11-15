a0723e5a06565155df15d8042104cbae1dc844fc
joão vitor <shepherdcom12@gmail.com>
2023-11-15 13:06:12 -0300

    remove heaader fixed

--- src/components/header/script.js
+++ src/components/header/script.js
@@ -1,12 +1,2 @@
-let scrollY = window.pageYOffset;
-let header = document.getElementById("header")
 
-window.addEventListener('scroll', function (e) {
-    scrollY = window.pageYOffset;
-    if (scrollY > 100) {
-        header.classList.add("active")
-    } else {
-        header.classList.remove("active")
-    }
-})
 
